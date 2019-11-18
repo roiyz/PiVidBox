8ad27661f8e04de230f841dbfca542358a49f032
Roiy Zysman <roiy.zysman@gmail.com>
2019-11-17 18:09:56 -0800

    initial commmit

---
+++ src/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+OIFS="$IFS"
+IFS=$'\n'
+echo "starting" > /tmp/log.txt
+sleep 10
+while true
+do
+	for file in `find /media/pi -type f \( -iname \*.avi -o -iname \*.mp4 \) | shuf`
+                do
+			echo "$file"
+                        echo "$file" >>/tmp/out.txt
+			/usr/bin/omxplayer --adev hdmi --aspect-mode stretch --threshold 5 $file
+                done
+	echo "Completed cycle" >>/tmp/out.txt
+	sleep 10
+done
+
